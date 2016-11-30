3658800d560b09f3593558379efb2d9e00d7d07b
Gabin <hello@gabinaureche.com>
2016-11-30 23:23:43 +0100

    add hidden labels

--- src/content.js
+++ src/content.js
@@ -1,4 +1,3 @@
-import parallel from 'async/parallel';
 import minimatch from 'minimatch';
 import storage from './lib/storage';
 import { GET_PROJECT_NAME, RULES_CHANGE } from './lib/actions';
@@ -12,29 +11,22 @@ chrome.runtime.onMessage.addListener((action, sender, respond) => {
   }
 });
 
-parallel([
-  (callback) => {
-    storage.getRules(projectName, (rules) => {
-      callback(null, rules);
-    });
-  },
-  (callback) => {
-    const wrapper = document.getElementById('pr-tab-content');
-    const observer = new MutationObserver(() => {
-      callback(null, wrapper);
-      observer.disconnect();
-    });
+const wrapper = document.getElementById('pr-tab-content');
+const observer = new MutationObserver(() => {
+  observer.disconnect();
 
-    observer.observe(wrapper, {
-      childList: true,
-      subtree: true
-    });
-  }
-], (err, [rules, wrapper]) => {
-  const files = Array.prototype.slice.call(wrapper.querySelectorAll('.commentable-diff'));
-  updateHiddenFiles(files, rules);
+  const listOfChangedFiles = Array.prototype.slice.call(wrapper.querySelectorAll('.file'));
+  listOfChangedFiles.forEach((file) => {
+    const label = document.createElement('span');
+    label.setAttribute('data-hidden-label', '1');
+    label.style.display = 'none';
+    label.style.color = '#707070';
+    label.textContent = '(hidden)';
+    file.appendChild(label);
+  });
 
-  files.forEach((file) => {
+  const filesDiff = Array.prototype.slice.call(wrapper.querySelectorAll('.commentable-diff'));
+  filesDiff.forEach((file) => {
     const path = file.getAttribute('data-path');
 
     const addRuleButton = document.createElement('button');
@@ -47,33 +39,67 @@ parallel([
         return;
       }
 
-      storage.addRule(projectName, newRule);
-      file.style.display = 'none';
+      storage.addRule(projectName, newRule, updateHiddenFilesFromStorage);
     });
 
     file.querySelector('.aui-buttons').appendChild(addRuleButton);
   });
 
+  updateHiddenFilesFromStorage();
+
   chrome.runtime.onMessage.addListener((action) => {
     if (action.type === RULES_CHANGE) {
-      storage.getRules(projectName, (rules) => {
-        updateHiddenFiles(files, rules);
-      });
+      updateHiddenFilesFromStorage();
     }
   });
+
+  function updateHiddenFilesFromStorage() {
+    storage.getRules(projectName, (rules) => {
+      if (hasUser(projectName)) {
+        const user = getUser(projectName);
+        storage.getRules(user, (userRules) => {
+          updateHiddenFilesElements(userRules.concat(rules));
+        });
+      } else {
+        updateHiddenFilesElements(rules);
+      }
+    });
+  }
+
+  function updateHiddenFilesElements(rules) {
+    listOfChangedFiles.forEach((file) => {
+      const path = file.getAttribute('data-file-identifier');
+      if (isIgnored(path, rules)) {
+        file.querySelector('[data-hidden-label]').style.display = '';
+      } else {
+        file.querySelector('[data-hidden-label]').style.display = 'none';
+      }
+    });
+
+    filesDiff.forEach((file) => {
+      const path = file.getAttribute('data-path');
+      if (isIgnored(path, rules)) {
+        file.style.display = 'none';
+      } else {
+        file.style.display = '';
+      }
+    });
+  }
+
+  function hasUser(projectName) {
+    return projectName.indexOf('/') > 0;
+  }
+
+  function getUser(projectName) {
+    return projectName.split('/')[0];
+  }
+
+  function isIgnored(path, rules) {
+    return rules.some((rule) => minimatch(path, rule));
+  }
 });
 
-function updateHiddenFiles(files, rules) {
-  files.forEach((file) => {
-    const path = file.getAttribute('data-path');
-    if (isIgnored(path, rules)) {
-      file.style.display = 'none';
-    } else {
-      file.style.display = '';
-    }
-  });
-}
-
-function isIgnored(path, rules) {
-  return rules.some((rule) => minimatch(path, rule));
-}
+observer.observe(wrapper, {
+  childList: true,
+  subtree: true
+});
